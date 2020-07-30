47792927b2039fdff66c43c57b202b93a8ccb0b7
nabilhayet <nabil4457@yahoo.com>
2020-07-30 14:44:37 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,10 +12,9 @@ class SchoolClassesController < ApplicationController
 	end
 
     def create
-        binding.pry 
       @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
       @school_class.save 
-	  redirect_to schoolclass_path(@school_class)
+	  redirect_to school_class_path(@school_class)
 	end
 
 	def edit
@@ -25,6 +24,6 @@ class SchoolClassesController < ApplicationController
 	def update
 	  @school_class = SchoolClass.find(params[:id])
 	  @school_class.update(params.require(:school_class).permit(:title, :room_number))
-	  redirect_to schoolclass_path(@school_class)
+	  redirect_to school_class_path(@school_class)
 	end
 end 
