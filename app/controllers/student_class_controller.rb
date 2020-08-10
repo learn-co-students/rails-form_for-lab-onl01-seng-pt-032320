6a549c3bdf7467ea1db496454c07724ed5fa7ab5
Vlad <VladDaImpaler@gmail.com>
2020-08-10 03:28:50 -0400

    Done.

---
+++ app/controllers/student_class_controller.rb
@@ -0,0 +1,2 @@
+class StudentClassController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'student_class/new'
+
+  get 'student_class/create'
+
+  get 'student_class/show'
+
+  get 'student_class/edit'
+
+  get 'student_class/update'
+
+  get 'students/new'
+
+  get 'students/create'
+
+  get 'students/show'
+
+  get 'students/edit'
+
+  get 'students/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
