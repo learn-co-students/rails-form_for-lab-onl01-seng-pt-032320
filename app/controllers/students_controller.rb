649c1919fdcb57c89bf31e8d6b6662236233a984
Bryce B <bryce.boddy@gmail.com>
2020-08-15 08:00:43 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,29 +1,29 @@
 class StudentsController < ApplicationController
   def index
-    @students = student.all
+    @students = Student.all
   end
 
   def show
-    @student = student.find(params[:id])
+    @student = Student.find(params[:id])
   end
 
   def new
-    @student = student.new
+    @student = Student.new
   end
 
   def create
-    @student = student.new(params.require(:student).permit(:first_name, :last_name))
+    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
   end
    
   def update
-    @student = student.find(params[:id])
+    @student = Student.find(params[:id])
     @student.update(params.require(:student).permit(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
   def edit
-    @student = student.find(params[:id])
+    @student = Student.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :show, :new, :create, :edit, :update]
+  resources :school_classes, except: [:destroy, :index]
 end
