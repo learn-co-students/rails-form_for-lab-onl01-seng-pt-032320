1ca40c73060c1873c04af614ae0d451c2dc498f3
kellerns <nic.keller328@gmail.com>
2020-09-06 18:59:38 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,13 +1,9 @@
 class SchoolClassesController < ApplicationController
 
-    def index
-        @school_classes = SchoolClass.all
-    end
-
-    def show
-        @school_class = SchoolClass.find(parmas[:id])
-    end
-
+    # def index
+    #     @school_classes = SchoolClass.all
+    # end
+    
     def new
         @school_class = SchoolClass.new
     end
@@ -16,10 +12,18 @@ class SchoolClassesController < ApplicationController
         @school_class = SchoolClass.create(coupon_code: params[:school_class][:title], room_number: params[:school_class][:room_number])
         redirect_to school_class_path(@school_class)
     end
+    
+    def show
+        @school_class = SchoolClass.find(parmas[:id])
+    end
 
     def edit
+        @school_class = SchoolClass.find(params[:id])
     end
 
     def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(params.require(:school_class).permit(:title, :room_number))
+        redirect_to school_class_path(@school_class)
     end
 end
